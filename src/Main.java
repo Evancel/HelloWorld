dd0727d7c7b1cb83b39b4cdace255d313057554f
wlexa <w.lexandra@gmail..com>
2022-12-16 19:29:54 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! It's me");
+        System.out.println("Hello world! It's me again");
     }
 }
